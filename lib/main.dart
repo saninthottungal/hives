9e660256d957d39581047808e4f2ae797239646a
sanin <saninthottungalhere@gmail.com>
2025-02-07 22:00:24 +0530

    dependecies updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final dir = await getApplicationCacheDirectory();
+
   runApp(const MyApp());
 }
 
